7a52d28508158268368ad5ee3eddf1ee08146a39
JoeunNAL <euji42@gmail.com>
2023-02-07 15:05:26 +0900

    design: navbar, 메인페이지 반응형 적용

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -1,30 +1,27 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-  <a class="navbar-brand" href="#">올해의 책</a>
+  <span class="navbar-brand">올해의 책</span>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
 
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item active ">
-        <a class="nav-link" href={{route("home.index")}}>추천 목록</a>
-        <!-- <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a> -->
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href={{route("book.create")}}>등록</a>
+    <ul class="navbar-nav ml-md-auto text-center min-width-150">
+      <li class="nav-item mx-3 row min-width-150">
+        <a class="nav-link col-lg-8 col-12" href={{route("home.index")}}>추천 목록</a>
+        <a class="nav-link col-lg-4 col-12" href={{route("book.create")}}>등록</a>
       </li>
       @if(Auth::user())
-      <li class="nav-item">
-        <div class="text-white">{{ Auth::user() -> name }}</div>
+      <li class="nav-item mx-3 mt-3 my-auto mb-2 mb-md-0">
+        <div class="text-info mb-2">{{ Auth::user() -> name }} 님</div>
       </li>
-      <li class="nav-item">
+      <li class="nav-item mx-lg-1 mb-2 mb-lg-0">
         <form action="/logout"method="POST">
           @csrf
           <button type="submit" class="btn btn-light">로그아웃</button>
         </form>
       </li>
       @else
-      <li class="nav-item">
+      <li class="nav-item mb-2 mb-lg-0">
         <button class="btn btn-light" onclick="location.href = '{{ route("login") }}'">로그인</button>
       </li>
       @endif
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,96 +1,98 @@
-@extends('layout')
+@extends("layout")
 
-@section('content')
-  <section class='book-table'>
-    <h1 class='my-4'>올해의 추천</h1>
-    <form action='/search' method='GET' class='d-flex mb-4 search-select-container justify-content-between'>
-      <div class='flex-column justify-content-between container'>
-        <div class='d-flex'>
-          <label for='search' class='flex-shrink-0 me-3'>카테고리 : </label>
-          <select name='selected_category' class='form-select form-select-sm mb-4 flex-grow-1' id='search' aria-lfabel='.form-select-sm example'>
-              <option value=''>카테고리 선택</option>
-              @foreach($category_names as $category)
-                <option value='{{ $category }}' {{ request() -> get('selected_category') === $category ? 'selected' : null }}>{{ $category }}</option>
-              @endforeach
-          </select>
-        </div>
-        <div class='d-flex'>
-            <label for='search' class='flex-shrink-0 me-3'>브랜드 : </label>
-            <select name='selected_brand' class='form-select form-select-sm flex-grow-1' id='search' aria-label='.form-select-sm example'>
-              <option value=''>브랜드 선택</option>
-              @foreach($brands as $brand)
-                <option value='{{ $brand -> id }}' {{ request() -> get('selected_brand') == ($brand->id) ? 'selected' : null }}>{{ $brand -> brand_name }}</option>
-              @endforeach
+@section("content")
+  <main class="container-md">
+    <h1 class="text-center text-sm-left my-5">올해의 추천</h1>
+    <section class="row">
+      <form action="/search" method="GET" class="col-12 col-lg-7 justify-content-between row m-0 p-0 mb-4">
+        <div class="col-12 col-md-9">
+          <div class="row m-0 mb-3">
+            <label for="search" class="col-12 col-sm-2 col-md-3 p-0">카테고리 : </label>
+            <select name="selected_category" class="col-12 col-sm-10 col-md-9 custom-select" id="search" aria-lfabel=".form-select-sm example">
+                <option value="">카테고리 선택</option>
+                @foreach($category_names as $category)
+                <option value="{{ $category }}" {{ request() -> get("selected_category") === $category ? "selected" : null }}>
+                  {{ $category }}
+                </option>
+                @endforeach
             </select>
+          </div>
+          <div class="row m-0">
+              <label for="search" class="col-12 col-sm-2 col-md-3 p-0">브랜드 : </label>
+              <select name="selected_brand" class="col-12 col-sm-10 col-md-9 custom-select" id="search" aria-label=".form-select-sm example">
+                <option value="">브랜드 선택</option>
+                @foreach($brands as $brand)
+                  <option value="{{ $brand -> id }}" 
+                      {{ request() -> get("selected_brand") == ($brand->id) ? "selected" : null }}>
+                      {{ $brand -> brand_name }}
+                  </option>
+                @endforeach
+              </select>
+          </div>
         </div>
-      </div>
-      <button type='submit' class='btn btn-light'>검색</button>
-    </form>
-
-    <table class='table table-hover'>
-      <thead>
-        <tr>
-          <td>No.</td>
-          <td>제목</td>
-          <td>저자</td>
-          <td>페이지</td>
-          <td>가격</td>
-          <td>카테고리</td>
-          <td>브랜드</td>
-          <td>수정</td>
-        </tr>
-      </thead>
-      <tbody class='table-group-divider'>
-        @foreach($books as $book)
-          <tr>
-            <td>{{$book->id}}</td>
-            <td>{{$book->title}}</td>
-            <td>
-              @if(!$book->author)
-                -
-              @else
-                {{ $book->author }}
-              @endif
-            </td>
-            <td>{{$book->page}}</td>
-            <td>{{$book->price}}</td>
-            <td>{{$book->category->name}}</td>
-            <td>{{$book->category->brand->brand_name}}</td>
-            <td>
-              @if($user_recommends -> where('book_id', $book->id) -> first())
-                <button type='button' class='btn btn-light' 
-                    onclick='location.href = "/book/{{ $book -> id }}/edit"'
-                >수정</button>
-              @endif 
-            </td>
-            <td>
-              <button type='button' class='active-log-search-btn btn btn-outline-secondary' data-route='/book/{{ $book -> id }}/log' data-bs-toggle='modal' data-bs-target='#exampleModal'>로그 조회</button>
-            </td>
-          </tr>
-        @endforeach
-      </tbody>
-    </table>
-  </section>
-  <section class='modal fade' id='exampleModal' tabindex='-1' aria-labelledby='exampleModalLabel' aria-hidden='true'>
-    <div class='modal-dialog'>
-      <div class='modal-content'>
-        <div class='modal-header'>
-          <h2 class='modal-title' id='exampleModalLabel'>로그내역</h2>
-          <button type='button' class='btn-close' data-bs-dismiss='modal' aria-label='Close'></button>
+        <div class="col-12 col-md-3 my-3 my-md-0">
+          <button type="submit" class="btn btn-light w-100 h-100">검색</button>
         </div>
-        <div class='modal-body'>
-        <table class='table table-hover'>
+      </form>
+    </section>
+    <section class="table-responsive-xl row m-0">
+        <table class="table table-hover col-12">
           <thead>
             <tr>
-              <td>일/시</td>
-              <td>내역</td>
+              <th scope="col">No.</th>
+              <th scope="col">제목</th>
+              <th scope="col">저자</th>
+              <th scope="col">페이지</th>
+              <th scope="col">가격</th>
+              <th scope="col">카테고리</th>
+              <th scope="col">브랜드</th>
+              <th scope="col">수정</th>
+              <th scope="col"></th>
             </tr>
           </thead>
-          <tbody id='log-table' class='table-group-divider'></tbody>
+          <tbody class="table table-hover">
+            @foreach($books as $book)
+              <tr>
+                <td>{{$book->id}}</td>
+                <td class="max-width-200">
+                  <div class="ellipsis-1">{{$book->title}}</div>
+                </td>
+                <td class="max-width-150" scope="row">
+                  <div class="ellipsis-1">
+                    @if(!$book->author)
+                      -
+                    @else
+                      {{ $book->author }}
+                    @endif
+                  </div>
+                </td>
+                <td class="max-width-80">
+                  <div class="ellipsis-1">{{$book->page}}</div>
+                </td>
+                <td class="max-width-80">
+                  <div class="ellipsis-1">{{$book->price}}</div>
+                </td>
+                <td class="min-width-100">{{$book->category->name}}</td>
+                <td class="min-width-100">{{$book->category->brand->brand_name}}</td>
+                <td>
+                  @if($user_recommends -> where("book_id", $book->id) -> first())
+                  <button type="button" class="btn btn-light min-width-60" 
+                      onclick="location.href = '/book/{{ $book -> id }}/edit'">
+                      수정
+                  </button>
+                  @endif 
+                </td>
+                <td>
+                  <button type="button" class="min-width-60 text-wrap active-log-search-btn btn btn-outline-secondary" 
+                    data-route="/book/{{ $book -> id }}/log" data-toggle="modal" data-target="#exampleModal">
+                    로그 조회
+                  </button>
+                </td>
+              </tr>
+            @endforeach
+          </tbody>
         </table>
-        </div>
-      </div>
-    </div>
-  </section>
-  <script src='assets/logModal.js'></script>
+    </section>
+  </main>
+  @include("modal")
 @endsection
